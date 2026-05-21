a62d5c404997a5a68194e208bd2ecc3bdeae4cd2
Arunima Anand <arunima.anand25@gmail.com>
2026-05-21 10:15:19 -0700

    Fix Lab 8 q1_7 expected value

--- labs/Lab8/tests/q1_7.R
+++ labs/Lab8/tests/q1_7.R
@@ -7,7 +7,7 @@ test = list(
       points = 5.0,
       code = {
         testthat::expect_type(perm_pval_q17, "double")
-        testthat::expect_equal(as.numeric(perm_pval_q17), 0.3015, tolerance = 1e-10)
+        testthat::expect_equal(as.numeric(perm_pval_q17), 0.3575, tolerance = 1e-10)
         
 
       }
